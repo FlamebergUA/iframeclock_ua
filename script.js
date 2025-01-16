46d78130a80bef602a602a909466683b038378a3
FlamebergUA <flamebergua@gmail.com>
2025-01-16 20:10:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-function date() {
-    var today = new Date();
-    var options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    };
-    document.getElementById('date').innerHTML = today.toLocaleDateString('en-GB', options);
-}
-
-function clock() {
-    var today = new Date();
-    var hour = zeros(today.getHours());
-    var minutes = zeros(today.getMinutes());
-    var seconds = zeros(today.getSeconds());
-    document.getElementById('hour').innerHTML = hour;
-    document.getElementById('min').innerHTML = minutes;
-    document.getElementById('sec').innerHTML = seconds;
-}
-
-// Adds zero in front of single-digit number
-function zeros(num) {
-    if (num < 10) {
-        num = '0' + num;
+document.addEventListener("DOMContentLoaded", function() {
+    function date() {
+        var today = new Date();
+        var options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        };
+        document.getElementById('date').innerHTML = today.toLocaleDateString('en-GB', options);
     }
-    return num;
-}
 
-function dateTime() {
-    date();
-    clock();
-    setTimeout(dateTime, 500);
-}
+    function clock() {
+        var today = new Date();
+        var hour = zeros(today.getHours());
+        var minutes = zeros(today.getMinutes());
+        var seconds = zeros(today.getSeconds());
+        document.getElementById('hour').innerHTML = hour;
+        document.getElementById('min').innerHTML = minutes;
+        document.getElementById('sec').innerHTML = seconds;
+    }
 
-dateTime();
-// END
+    // Adds zero in front of single-digit number
+    function zeros(num) {
+        if (num < 10) {
+            num = '0' + num;
+        }
+        return num;
+    }
+
+    function dateTime() {
+        date();
+        clock();
+        setTimeout(dateTime, 500);
+    }
+
+    dateTime();
+});
